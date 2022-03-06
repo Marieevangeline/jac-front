0dced9bb649f26ad6d184244ba08137a844f5214
Marieevangeline <medelabroise@juniorisep.com>
2022-03-06 22:53:47 +0100

    2commit06/03/2022

--- composants/barre de recherche.ts
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-import {text} from 'react-native';
-const 
---
+++ src/Views/SignupView.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, ScrollView, View, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator} from '@react-navigation/native-stack';
+import Style from '../../Style';
+
+function signupview() {
+    return (
+        <View style={Style.container}>
+            <text>Ecran d'inscription </text>
+        </View>
+    );
+  }
+  export default signupview
