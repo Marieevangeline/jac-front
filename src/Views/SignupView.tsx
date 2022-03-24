321990aa8dd2ad003d4fc6916254e8a04c4ddc3b
Marieevangeline <medelabroise@juniorisep.com>
2022-03-24 16:48:35 +0100

    style

--- src/Views/SignupView.tsx
+++ src/Views/SignupView.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 import { Text, ScrollView, View, Button, RefreshControlBase } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator} from '@react-navigation/native-stack';
 import Style from '../../Style';
 
 function signupview() {
