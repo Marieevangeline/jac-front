ca12d25caa31cb8c9ffba974bf0614f4be30b873
Marieevangeline <medelabroise@juniorisep.com>
2022-04-02 23:10:24 +0200

    change mot de passe

--- src/Views/Changemotdepasse.tsx
+++ src/Views/Changemotdepasse.tsx
@@ -7,12 +7,13 @@ function ForgetPassword({navigation}:any) {
     return(
         <View style={Style.container}> 
             <Image source={require('../../assets/logo_UNICEF.png')} />
-            <Text>Mot de passe oublié ?</Text>
-            <Text>Saisissez l'adresse e-mail associée à votre compte</Text>
+            <Text style={{fontSize:23,fontFamily:'Poppins',fontWeight:'bold',margin:20,marginRight:55}}>Mot de passe oublié ?</Text>            <Text>Saisissez l'adresse e-mail associée à votre compte</Text>
             <EmailTextInput/>
-           
+            <Text style={{margin:20}}>Saisissez l'adresse e-mail associée à votre compte</Text>
             <Button title="Valider" onPress={() => navigation.navigate('SignUp')} />
-            <Button title="Annuler" onPress={() => navigation.navigate('Login')} />
+            <View style={{marginVertical:20, flexDirection:"row"}}>
+              <Text onPress={() => navigation.navigate('Login')} style={Style.text_button}> Annuler </Text>
+            </View>
         </View>
     )
 }
